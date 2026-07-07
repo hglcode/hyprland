b3fa087f12602ef381a497444f08e14b189fb06a
sam <hgl9@foxmail.com>
2026-07-07 19:58:57 +0800

    wip: set display backlight default value as 60.

--- scripts/backlight.sh
+++ scripts/backlight.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -u
+set -ue
 
 [ $# -lt 1 ] && exit 1
 
@@ -9,5 +9,5 @@ if [ "$1" = "off" ]; then
     ddcutil getvcp 10 | cut -d'=' -f2 | cut -d',' -f1 | tr -d ' ' | tee /tmp/monitor/backlight
     ddcutil setvcp 10 1
 else
-    ddcutil setvcp 10 "$(cat /tmp/monitor/backlight 2>/dev/null || echo 66)"
+    ddcutil setvcp 10 "$(cat /tmp/monitor/backlight 2>/dev/null || echo 60)"
 fi
